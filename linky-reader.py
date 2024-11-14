1d73218e5213b554bc3d9ec840474a84518c7f80
Tony Chouteau <tony.chouteau@outlook.fr>
2024-11-14 01:33:20 +0100

    trying to fix formatting

--- linky-reader.py
+++ linky-reader.py
@@ -14,24 +14,24 @@ log_file = '/home/pi/tic_data.log'
 linky_code = {
     "ADCO": {
         "name": "Identifiant du compteur",
-        "format": lambda data: data.split("ADCO ")[1].split(" @")[0]
+        "format": lambda data: data.split(" ")[1]
     },  # The meter's address (a unique identifier for the meter)
     "OPTARIF": {
         "name": "Option tarifaire choisie",
-        "format": lambda data: data.split("OPTARIF ")[1].split(" <")[0]
+        "format": lambda data: data.split(" ")[1]
     },  # The selected tariff option (e.g., HC/HP, EJP, Tempo)
     "ISOUSC": {
         "name": "Intensité souscrite",
-        "format": lambda data: data.split("ISOUSC ")[1].split(" ?")[0]
+        "format": lambda data: data.split(" ")[1]
     },  # The subscribed intensity (maximum power limit)
     # "BASE": "Index option Base",        # The base consumption index (for standard tariff)
     "HCHC": {
         "name": "Index Heures Creuses",
-        "format": lambda data: data.split("HCHC ")[1].split(" )")[0]
+        "format": lambda data: data.split(" ")[1]
     },   # The consumption index for off-peak hours (Heures Creuses)
     "HCHP": {
         "name": "Index Heures Pleines",
-        "format": lambda data: data.split("HCHP ")[1].split(" 6")[0]
+        "format": lambda data: data.split(" ")[1]
     },  # The consumption index for peak hours (Heures Pleines)
     # "EJPHN": "Index option EJP Heures Normales",     # The index for EJP normal hours
     # "EJPHPM": "Index option EJP Heures de Pointe Mobile",    # The index for EJP peak mobile hours
@@ -44,29 +44,29 @@ linky_code = {
     # "PEJP": "Préavis Début EJP",      # Notification for the beginning of the EJP (Electricité Jour de Pointe) period
     "PTEC": {
         "name": "Période Tarifaire en cours",
-        "format": lambda data: data.split("PTEC ")[1].split(" ")[0]
+        "format": lambda data: data.split(" ")[1]
     },  # The current active tariff period (e.g., full hours or part of it)
     # "DEMAIN": "Couleur du lendemain",  # Color of the next day (for the Tempo tariff, indicating the next day’s type)
     "IINST": {
         "name": "Intensité Instantanée",
-        "format": lambda data: data.split("IINST ")[1].split(" \\")[0]
+        "format": lambda data: data.split(" ")[1]
     },  # The instantaneous intensity (current power usage)
     # "ADPS": "Avertissement de Dépassement De Puissance Souscrite",  # Warning for exceeding the subscribed power limit
     "IMAX": {
         "name": "Intensité maximale appelée",
-        "format": lambda data: data.split("IMAX ")[1].split(" H")[0]
+        "format": lambda data: data.split(" ")[1]
     },  # The maximum intensity (current highest power demand)
     "PAPP": {
         "name": "Puissance apparente",
-        "format": lambda data: data.split("PAPP ")[1].split(" &")[0]
+        "format": lambda data: data.split(" ")[1]
     },  # Apparent power (combination of active and reactive power)
     "HHPHC": {
         "name": "Horaire Heures Pleines Heures Creuses",
-        "format": lambda data: data.split("HHPHC ")[1].split(" ,")[0]
+        "format": lambda data: data.split(" ")[1]
     },  # The time schedule for peak and off-peak hours
     "MOTDETAT": {
         "name": "Mot d'état du compteur",
-        "format": lambda data: data.split("MOTDETAT ")[1].split(" B")[0]
+        "format": lambda data: data.split(" ")[1]
     }   # Meter status message (e.g., normal operation or fault)
 }
 
