26e2ef00ab56ecba56f004611314ffb5e66c16d2
Tony Chouteau <tony.chouteau@outlook.fr>
2024-11-14 02:04:23 +0100

    adding postgresql save for TIC output

--- linky-reader.py
+++ linky-reader.py
@@ -1,12 +1,26 @@
 import serial
 import time
-import sqlite3
+from datetime import datetime
+import psycopg2
+from psycopg2 import sql
+from datetime import datetime
+
+# Connect to your PostgreSQL database
+with open("db_login.txt") as f:
+    lines = f.readlines()
+    conn = psycopg2.connect(
+        dbname=lines[0],    # Replace with your database name
+        user=lines[1],      # Replace with your username
+        password=lines[2],  # Replace with your password
+        host=lines[3],      # Replace with your host, if different
+        port=lines[4]       # Replace with your PostgreSQL port, if different
+    )
 
 # Configure the serial port (ensure it matches your setup)
 ser = serial.Serial('/dev/ttyAMA0', 1200, timeout=1)
 ser.bytesize = 7       # Set the data bits to 7
-ser.parity   = serial.PARITY_EVEN  # Set even parity
-ser.stopbits = serial.STOPBITS_ONE # Set stop bits to 1
+ser.parity = serial.PARITY_EVEN  # Set even parity
+ser.stopbits = serial.STOPBITS_ONE  # Set stop bits to 1
 
 # Log file where the TIC data will be saved
 log_file = '/home/pi/tic_data.log'
@@ -22,16 +36,16 @@ linky_code = {
     },  # The selected tariff option (e.g., HC/HP, EJP, Tempo)
     "ISOUSC": {
         "name": "Intensité souscrite",
-        "format": lambda data: data.split(" ")[1] + "Wh"
+        "format": lambda data: data.split(" ")[1]
     },  # The subscribed intensity (maximum power limit)
     # "BASE": "Index option Base",        # The base consumption index (for standard tariff)
     "HCHC": {
         "name": "Index Heures Creuses",
-        "format": lambda data: data.split(" ")[1] + "Wh"
+        "format": lambda data: data.split(" ")[1]
     },   # The consumption index for off-peak hours (Heures Creuses)
     "HCHP": {
         "name": "Index Heures Pleines",
-        "format": lambda data: data.split(" ")[1] + "Wh"
+        "format": lambda data: data.split(" ")[1]
     },  # The consumption index for peak hours (Heures Pleines)
     # "EJPHN": "Index option EJP Heures Normales",     # The index for EJP normal hours
     # "EJPHPM": "Index option EJP Heures de Pointe Mobile",    # The index for EJP peak mobile hours
@@ -49,16 +63,16 @@ linky_code = {
     # "DEMAIN": "Couleur du lendemain",  # Color of the next day (for the Tempo tariff, indicating the next day’s type)
     "IINST": {
         "name": "Intensité Instantanée",
-        "format": lambda data: data.split(" ")[1] + "A"
+        "format": lambda data: data.split(" ")[1]
     },  # The instantaneous intensity (current power usage)
     # "ADPS": "Avertissement de Dépassement De Puissance Souscrite",  # Warning for exceeding the subscribed power limit
     "IMAX": {
         "name": "Intensité maximale appelée",
-        "format": lambda data: data.split(" ")[1] + "A"
+        "format": lambda data: data.split(" ")[1]
     },  # The maximum intensity (current highest power demand)
     "PAPP": {
         "name": "Puissance apparente",
-        "format": lambda data: data.split(" ")[1] + "VA"
+        "format": lambda data: data.split(" ")[1]
     },  # Apparent power (combination of active and reactive power)
     "HHPHC": {
         "name": "Horaire Heures Pleines Heures Creuses",
@@ -70,6 +84,7 @@ linky_code = {
     }   # Meter status message (e.g., normal operation or fault)
 }
 
+values = []
 
 # Open the log file for appending data
 with open(log_file, 'a') as log:
@@ -84,14 +99,66 @@ with open(log_file, 'a') as log:
 
             tag_found = 0
             tag = None
-            for key in linky_code:
-                if key in data_decoded:
+            value = None
+            key = None
+            for _key in linky_code:
+                if _key in data_decoded:
                     tag_found += 1
-                    tag = linky_code[key]["name"] + " : " + linky_code[key]["format"](data_decoded)
+                    key = _key
+                    tag = linky_code[key]["name"]
+                    value = linky_code[key]["format"](data_decoded)
             if tag is None:
                 tag = "N/A"
+                value = None
+                key = None
 
-            log_entry = f"{tag_found}] {tag} | {timestamp} - {data_decoded}"
+            if key == "ADCO":
+                cur = conn.cursor()
+                insert_query = """
+                    INSERT INTO linky_data (
+                        ADCO, ADCO_timestamp,
+                        OPTARIF, OPTARIF_timestamp,
+                        ISOUSC, ISOUSC_timestamp,
+                        HCHC, HCHC_timestamp,
+                        HCHP, HCHP_timestamp,
+                        PTEC, PTEC_timestamp,
+                        IINST, IINST_timestamp,
+                        IMAX, IMAX_timestamp,
+                        PAPP, PAPP_timestamp,
+                        HHPHC, HHPHC_timestamp,
+                        MOTDETAT, MOTDETAT_timestamp
+                    ) 
+                    VALUES (
+                        %s, %s,
+                        %s, %s,
+                        %s, %s,
+                        %s, %s,
+                        %s, %s,
+                        %s, %s,
+                        %s, %s,
+                        %s, %s,
+                        %s, %s,
+                        %s, %s,
+                        %s, %s
+                    );
+                """
+
+                # Execute the query with the provided values
+                cur.execute(insert_query, values)
+
+                # Commit the transaction
+                conn.commit()
+
+                # Close the cursor and connection
+                cur.close()
+                # conn.close()
+
+                values = []
+
+            current_line.append(value)
+            current_line.append(datetime.now())
+
+            log_entry = f"{tag} : {value} | {timestamp} - {data_decoded}"
             log.write(log_entry)
 
             # Print to console as well
